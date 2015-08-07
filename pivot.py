d0e7666618735c74647aab776030d2131f59060f
boazgurdin <boaz.gurdin@gmail.com>
2015-08-07 14:36:14 -0700

    pivot query to debug

--- pivot.py
+++ pivot.py
@@ -5,23 +5,86 @@ sc = SparkContext('local[4]', 'RedditPivot')
 sql = HiveContext(sc)
 
 #comments = sql.read.json('s3n://%s:%s@boazreddit/micro_fake.json' % (os.environ['AWS_ACCESS_KEY'], os.environ['AWS_SECRET_KEY']))
-comments = sql.read.json('data/RC_2015-01')
-
-pivot = sql.sql('''SELECT
-                        author,
-                        MIN(CAST((FROM_UNIXTIME(INT(created_utc))) AS TIMESTAMP)) AS first_post,
-                        MAX(CAST((FROM_UNIXTIME(INT(created_utc))) AS TIMESTAMP)) AS last_post,
-                        COUNT(*) AS total_posts
-                   FROM comments
-                   GROUP BY author
-                   HAVING total_posts>1''')
-#pivot.cache()
-
-# pivot2 = sql.sql('''SELECT author, COLLECT_SET(body) AS posts
+comments = sql.read.json('data/micro_fake.json')
+comments.registerTempTable('comments')
+# users = sql.sql('''SELECT
+#                     comments.author AS author,
+#                     comments.id AS first_post_id,
+#                     comments.body AS first_post_body,
+#                     comments.subreddit AS first_post_subreddit,
+#                     comments.ups AS first_post_ups,
+#                     comments.downs AS first_post_downs,
+#                     responses.responses AS first_post_responses,
+#                     responses.response_ups AS first_post_response_ups,
+#                     responses.response_downs AS first_post_response_downs,
+#                     responses.total_responses AS first_post_total_responses,
+#                     responses.avg_response_ups AS first_post_avg_response_ups,
+#                     responses.avg_response_downs AS first_post_avg_response_downs,
+#                     SUBSTR(comments.parent_id, 1, 2) AS parent_type,
+#                     user_pivot.first_post_datetime AS first_post_datetime,
+#                     user_pivot.last_post_datetime AS last_post_datetime,
+#                     user_pivot.post_ids AS post_ids,
+#                     user_pivot.post_dates AS post_dates,
+#                     user_pivot.subreddits AS subreddits,
+#                     user_pivot.total_subreddits AS total_subreddits,
+#                     user_pivot.total_posts AS total_posts
 #                     FROM comments
-#                     WHERE author='fake_author1'
-#                     GROUP BY author''')
+#                     JOIN
+#                         (SELECT
+#                             author,
+#                             MIN(CAST((FROM_UNIXTIME(INT(created_utc))) AS TIMESTAMP)) AS first_post_datetime,
+#                             MAX(CAST((FROM_UNIXTIME(INT(created_utc))) AS TIMESTAMP)) AS last_post_datetime,
+#                             COLLECT_LIST(id) AS post_ids,
+#                             COLLECT_LIST(body) AS posts,
+#                             COLLECT_LIST(CAST((FROM_UNIXTIME(INT(created_utc))) AS post_dates,
+#                             COLLECT_LIST(ups) AS ups,
+#                             COLLECT_LIST(downs) AS downs,
+#                             COLLECT_LIST(link_id) AS link_ids,
+#                             COLLECT_LIST(parent_id) AS parent_ids,
+#                             COLLECT_LIST(subreddit) AS subreddits,
+#                             COUNT(DISTINCT(subreddit)) AS total_subreddits,
+#                             COUNT(*) AS total_posts
+#                        FROM comments
+#                        GROUP BY author
+#                        HAVING total_posts>1) user_pivot
+#                     ON comments.author=user_pivot.author
+#                         AND CAST((FROM_UNIXTIME(INT(comments.created_utc))) AS TIMESTAMP)=user_pivot.first_post_datetime
+#                     JOIN
+#                        (SELECT
+#                             parent_id,
+#                             COLLECT_LIST(body) AS responses,
+#                             COLLECT_LIST(ups) AS response_ups,
+#                             COLLECT_LIST(downs) AS response_downs,
+#                             COUNT(*) AS total_responses,
+#                             AVG(ups) AS avg_response_ups,
+#                             AVG(downs) AS avg_response_downs
+#                         FROM
+#                             comments
+#                         GROUP BY
+#                             parent_id) responses
+#                         ON user_pivot.first_post_id=SUBSTR(responses.parent_id,4)''')
+
+responses = sql.sql('''SELECT
+                            parent_id,
+                            COLLECT_LIST(body) AS responses,
+                            COLLECT_LIST(ups) AS response_ups,
+                            COLLECT_LIST(downs) AS response_downs,
+                            COUNT(*) AS total_responses,
+                            AVG(ups) AS avg_response_ups,
+                            AVG(downs) AS avg_response_downs
+                        FROM
+                            comments
+                        GROUP BY
+                            parent_id
+                        HAVING
+                            total_responses>1
+                            ''')
+
+responses.registerTempTable('responses')
+responses.toJSON().saveAsTextFile('responses_test')
 
 
-pivot.toJSON().saveAsTextFile('users')
+#users.registerTempTable('users')
+#users.toJSON().saveAsTextFile('users_test')
+
 #comments.toJSON().saveAsTextFile('s3n://%s:%s@boazreddit/outtest' % (os.environ['AWS_ACCESS_KEY'], os.environ['AWS_SECRET_KEY']))
