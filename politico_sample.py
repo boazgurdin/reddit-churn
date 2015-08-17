96ff7043d4db9c6938f3953f2028dd3b8a446ac4
boazgurdin <boaz.gurdin@gmail.com>
2015-08-17 16:37:58 -0700

    balanced sample generator

--- politico_sample.py
+++ politico_sample.py
@@ -1,36 +1,71 @@
+from pyspark import SparkContext, SparkConf
+import os
+
+sp_conf = SparkConf()
+sp_conf.setAppName('PoliticoSampler')
+sp_conf.set('spark.driver.memory', '4g')
+sc = SparkContext(conf=sp_conf) #'local[4]', 'PoliticoExplorer')
+
+numPartitions = 16
+
+# Read full dataset of Reddit Politics users
+#politicos_read = sc.textFile('data/politicos/full')
+politicos_read = sc.textFile('s3n://boazreddit/politicos')
+politicos = politicos_read.repartition(numPartitions)
+
+# Downsample to 10% (max recommended for statistical inference)
+politicos_mixed_sample = politicos.sample(False, 0.10)
+
+# Label (single/multi) and calculate counts for balancing sample
+def single_post(line):
+    return '"total_posts":1' in line
+politicos_single_multi = politicos_mixed_sample.map(lambda line: (single_post(line), line))
+politicos_single_multi.cache()
+total_count = politicos_single_multi.count()
+single_count = politicos_single_multi.filter(lambda (single_post_status, line): single_post_status).count()
+multi_count = total_count - single_count
+
+# Balance samples
+# Ex: multi=0.7, single=0.3
+# single_sample_rate=1
+# multi_sample_rate = 0.3/0.7
+if multi_count >= single_count:
+    multi_sample_rate = single_count * 1.0 / multi_count
+    politicos_multi_post = politicos_single_multi \
+                               .filter(lambda (single_post_status, line): not single_post_status) \
+                               .sample(False, multi_sample_rate) \
+                               .map(lambda (single_post_status, line): line)
+    # keep all single posts
+    politicos_single_post = politicos_single_multi \
+                               .filter(lambda (single_post_status, line): single_post_status) \
+                               .map(lambda (single_post_status, line): line)
+
+else: # single_count > multi_count
+    single_sample_rate = multi_count * 1.0 / single_count
+    politicos_single_post = politicos_single_multi \
+                               .filter(lambda (single_post_status, line): single_post_status) \
+                               .sample(False, single_sample_rate) \
+                               .map(lambda (single_post_status, line): line)
+    # keep all multi posts
+    politicos_multi_post = politicos_single_multi \
+                               .filter(lambda (single_post_status, line): not single_post_status) \
+                               .map(lambda (single_post_status, line): line)
+
+# Write results
+politicos_single_post.saveAsTextFile('s3n://boazreddit/politicos_single_sample_balanced')
+#politicos_single_post.saveAsTextFile('data/politicos/politicos_single_sample_large')
+politicos_multi_post.saveAsTextFile('s3n://boazreddit/politicos_multi_sample_balanced')
+#politicos_single_post.saveAsTextFile('data/politicos/politicos_single_sample_large')
+
+sc.stop()
+
+
 # Sample size
 #
 # 1 sample        1 total     .7 multi
 # ----------   *  --------- * --------
 # 400 total      .7 multi    .3 single
 #
-# .0013 total
-# .0036 multi
-# .0083 single
-
-from pyspark import SparkContext, SparkConf
-from pyspark.sql import HiveContext
-import os
-
-sp_conf = SparkConf()
-sp_conf.setAppName('PoliticoSampler')
-sp_conf.set('spark.driver.memory', '3g')
-
-sc = SparkContext(conf=sp_conf) #'local[4]', 'PoliticoExplorer')
-sqlContext = HiveContext(sc)
-
-acc = os.environ['AWS_ACCESS_KEY_ID']
-sec = os.environ['AWS_SECRET_ACCESS_KEY']
-
-politicos = sqlContext.read.json('s3n://%s:%s@boazreddit/politicos/part-00000' % (acc, sec))
-politicos.cache()
-
-politicos.registerTempTable('politicos')
-politicos_mixed_post = politicos.sample(False, 0.001)
-politicos_mixed_post.toJSON().saveAsTextFile('s3n://%s:%s@boazreddit/politicos_mixed_sample' % (acc, sec))
-politicos_single_post = politicos.filter(politicos.total_posts==1).sample(False, 0.0083)
-politicos_single_post.toJSON().saveAsTextFile('s3n://%s:%s@boazreddit/politicos_single_sample' % (acc, sec))
-politicos_multi_post = politicos.filter(politicos.total_posts>1).sample(False, 0.0036)
-politicos_multi_post.toJSON().saveAsTextFile('s3n://%s:%s@boazreddit/politicos_multi_sample' % (acc, sec))
-
-sc.stop()
+# .0013 total  *12 = 0.0156
+# .0036 multi  *12 = 0.0432
+# .0083 single *12 = 0.0996
